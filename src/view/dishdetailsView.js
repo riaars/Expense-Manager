461ba78031a596dc53f6a5ac4449a3a0c4d67c8a
Magnus <MAGFRED@KTH.SE>
2019-11-14 22:51:49 +0100

    more views jsx

--- src/view/dishdetailsView.js
+++ src/view/dishdetailsView.js
@@ -1,3 +1,4 @@
+/* @jsx m*/
 class DishDetailsView {
     constructor(container, model) {
       this.container = container;
@@ -6,36 +7,27 @@ class DishDetailsView {
       this.recipeListView = undefined;
     }
   
-    render(dishId = 512) {
-      dishId = Math.floor(20 + Math.random() * Math.floor(512));
-      let innerHTML = 
-      `
-      <div class="grid-container" style="grid-template-rows: 0 auto;">
-        <div class="sidebar" id="sbv">sidebar</div>
-        <div class="main" id="recipecontainer"><h1><h1></div>
+    jsx = () => (
+      <div className="grid-container" style={{gridTemplateRows:"0 auto"}}>
+        <div className="sidebar" id="sbv">sidebar</div>
+        <div class="main" id="recipecontainer"><h1></h1></div>
         <div style="display:none" id="dishSearchView"></div>
       </div>
-      `
-      this.container.innerHTML = innerHTML;
-      if(this.sidebarView === undefined) {
+    ) 
+
+    render(dishId = 512) {
+      dishId = Math.floor(20 + Math.random() * Math.floor(512));
+      m.render(this.container, this.jsx());
+
+      if(this.sidebarView === undefined) 
         this.sidebarView = new SidebarView(this.container.querySelector("#sbv"), this.model);
-      }
       
-      if(this.recipeListView === undefined) {
+      
+      if(this.recipeListView === undefined) 
         this.recipeListView = new RecipeView(this.container.querySelector("#recipecontainer"), this.model, dishId);
-      }
-  
 
       this.afterRender();
     }
-    
-    dishDetailsInfo = () => {
-      return `
-      <div>Selected dish: Meatballs</div>
-      <img src="/../images/meatballs.jpg">
-      `
-    } 
-    
     
     afterRender() {
       this.sidebarView.render();
--- src/view/recipeView.js
+++ src/view/recipeView.js
@@ -1,3 +1,4 @@
+/* @jsx m*/
 class RecipeView {
     constructor (container, model, dishId) {
         this.container = container;
@@ -6,118 +7,97 @@ class RecipeView {
         this.dish = undefined;
     }
 
-    render() {
-        let innerHTML = `
-        <div class="recipeview-container">
-        <div class="recipe-view-row-col">
-        <h2><div id="dish-title">Dish Title</div></h2>
-            <div id="dish-image"></div>
-            <div class="dish-description" style="text-align:justify;  font-size:80%;">
+    jsx = () => (
+        <div className="recipeview-container">
+          <div className="recipe-view-row-col">
+          <h2><div id="dish-title">Dish Title</div></h2>
+          <div id="dish-image"></div>
+          <div class="dish-description" style="text-align:justify;  font-size:80%;">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             Vivamus sagittis, quam vitae lobortis pharetra, arcu felis 
             tempus nisl, a rhoncus nisl nisl quis orci. 
             Sed faucibus, sapien at lobortis placerat, nisl 
             velit feugiat nulla, vitae consectetur enim 
             libero ut tortor. Proin erat mauris, 
-            pretium eget sapien quis, auctor semper sem.</div>
-            <div class="startBtn" id="go-back-details">Back to search</div>
-            <div class="back-button"><h2>PREPARATION<h2></div>
-            <div id="instructions-container" style="font-size:80%;">
-            </div>
+            pretium eget sapien quis, auctor semper sem.
+          </div>
+          <div className="startBtn" 
+            id="go-back-details" 
+            onclick={() => {window.location.hash = '#search';}}>
+                Back to search
+          </div>
+          <div className="back-button"><h2>PREPARATION</h2></div>
+          <div id="instructions-container" style={{fontSize:"80%"}}></div>
         </div>
-        <div class="recipe-view-row-col">
-            
-            <div class="recipe-ingredients" style="padding:1em">
+        <div className="recipe-view-row-col">
+          <div className="recipe-ingredients" style={{padding:"1em"}}>
             <div>
-                <span>Igredients for &nbsp;</span>
-                <span class="value-num-guests"></span>
-                <span>&nbsp;people</span>
+              <span>Igredients for &nbsp;</span>
+              <span className="value-num-guests">{this.model.getNumberOfGuests()}</span>
+              <span>&nbsp;people</span>
             </div>
-            <div class="ingredients-inner" style="padding:1em;">
-                <hr class="horizontal-line">
-                <table class="ingredientslist-table" style="width:100%; padding-left:1em; padding-right:1em;">
-                </table>
-                <hr class="horizontal-line">
-                <div style="display:flex; flex-direction:row;
-                align-items: center;
-                justify-content: space-between;"
-                >
-                    <div class="startBtn" id="add-to-menu-button">
-                        Add to menu
-                    </div>
-                    <div>
-                        <span class="total price">SEK </span><span class="value-total-price" id="recipe-final-price"></span>
-                    </div>
-                </div>
+            <div className="ingredients-inner" style={{padding:"1em"}}>
+              <hr className="horizontal-line"/>
+              <table className="ingredientslist-table" style={{width:"100%", paddingLeft:"1em", paddingRight:"1em"}}>
+              </table>
+              <hr className="horizontal-line"/>
+              <div style={{display:"flex", 
+              flexDirection:"row",
+              alignItems:"center",
+              justifyContent: "space-between"}}>
+              <div className="startBtn" id="add-to-menu-button"
+              onclick={() =>{
+                this.model.getDish(this.dishId)
+                .then(dish => {
+                  this.model.addDishToMenu(dish);
+                  window.location.hash = '#search';}
+                )
+              }}>
+                Add to menu
+              </div>
+              <div>
+                <span className="total price">SEK </span><span className="value-total-price" id="recipe-final-price"></span>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
-        `
-        this.container.innerHTML = innerHTML;
+      </div>
+    </div>
+    )
+
+    render() {
+        m.render(this.container, this.jsx());
         this.afterRender();
     }
 
     afterRender() {
-        this.container.querySelector("#add-to-menu-button").addEventListener("click", () =>{
-            this.model.getDish(this.dishId).then(dish => {
-                this.model.addDishToMenu(dish);
-                window.location.hash = '#search';
-            })
-        });
-
         this.model.getDish(this.dishId).then( dish => {
             this.dish = dish;
             //Append the image and title
-            this.container.querySelector("#dish-title").innerHTML = dish.title;
-            let imgElem = document.createElement("img");
-            imgElem.setAttribute("src", dish.image);
-            imgElem.setAttribute("style", "width:100%;");
-            this.container.querySelector("#dish-image")
-            .appendChild(imgElem);
+            m.render(this.container.querySelector("#dish-title"), dish.title);
+            m.render(this.container.querySelector("#dish-image"),<img src={dish.image} style={{width:"100%"}}/>)
 
             //Append the instructions if there are any
             if(dish.analyzedInstructions[0]) {
-                
-                dish.analyzedInstructions[0].steps.map((stepInfo) => {
-                    let stepElem = document.createElement("div");
-                    let stepParagraph = document.createElement("p")
-                    stepElem.setAttribute("style", "padding-top:1em")
-                    stepParagraph.innerHTML = stepInfo.number +": " + stepInfo.step;
-                    stepElem.appendChild(stepParagraph);
-                    return stepElem;
-                }).forEach((elem) => {
-                    this.container.querySelector("#instructions-container").appendChild(elem);
-                });   
+              let stepsContent = dish.analyzedInstructions[0].steps.map((stepInfo) => (
+                <div style={{paddingTop:"1em"}}>
+                  <p>{stepInfo.number + ": " + stepInfo.step}</p>
+                </div>
+              ))
+              m.render(this.container.querySelector("#instructions-container"), stepsContent)
             } else 
-                this.container.querySelector("#instructions-container").innerHTML = "Instructions not clear, try sticking all the ingredients in a blender (API did not provide instructions)<br>Try reloading the page to get a new random dish!";
+              this.container.querySelector("#instructions-container").innerHTML = "Instructions not clear, try sticking all the ingredients in a blender (API did not provide instructions)<br>Try reloading the page to get a new random dish!";
             
             //Append the ingredients
-            "display: flex; justify-content: space-between;"
-            dish.extendedIngredients.map(ingredient => {
-                            //parse(parse()) => Max 2 dec, but remove trailing zeros
-                let textC1 = parseFloat(parseFloat(ingredient.measures.metric.amount).toFixed(2)) + " " + ingredient.measures.metric.unitShort 
-                let textC2 = (ingredient.meta[0] || "") + " "  + ingredient.name;
-                let colCreator = (text) => {
-                    let col = document.createElement("th")
-                    col.innerHTML = text;
-                    return col;
-                }
-                let rowElem = document.createElement("tr");
-                rowElem.appendChild(colCreator(textC1))
-                rowElem.appendChild(colCreator(textC2))
-                rowElem.appendChild(colCreator("SEK"))
-                rowElem.appendChild(colCreator("??"))
-                
-                
-                //textElement.innerHTML = text;
-                return rowElem; 
-            }).forEach(elem => {
-                this.container.getElementsByClassName("ingredientslist-table")[0].appendChild(elem);
-            })      
-            this.container.querySelector("#recipe-final-price").innerHTML = dish.pricePerServing*this.model.getNumberOfGuests();
+            let ingredientContent =  dish.extendedIngredients.map(ingredient => (
+              <tr>
+                <th>{parseFloat(parseFloat(ingredient.measures.metric.amount).toFixed(2)) + " " + ingredient.measures.metric.unitShort}</th>
+                <th>{(ingredient.meta[0] || "") + " "  + ingredient.name}</th>
+                <th>SEK</th>
+                <th>??</th>
+              </tr>
+            ))
+            m.render(this.container.getElementsByClassName("ingredientslist-table")[0], ingredientContent);
         })
-        this.container.getElementsByClassName("value-num-guests")[0].innerHTML = this.model.getNumberOfGuests();
-        this.container.querySelector("#go-back-details").addEventListener("click", () => {window.location.hash = '#search';} )
     }
 }
