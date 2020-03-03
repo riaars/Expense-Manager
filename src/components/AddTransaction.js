bcbb25951c228c2189db826c4ff2e5696b8e580d
Ria <riaratnasari@outlook.com>
2020-03-03 20:01:25 +0100

    add context

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
 
 const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+  const {addTransaction} = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form onSubmit={onSubmit} id="form">
         <div className="form-control">
           <label for="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
           <label for="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={e => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
